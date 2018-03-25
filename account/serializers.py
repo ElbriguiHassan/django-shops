fb4df4118f33e835d48b3f024ee3a86fb83016fb
ElbriguiHassan <hassanelbrigui@gmail.com>
2018-03-25 00:28:11 +0000

    Fix user signup serializer

--- account/serializers.py
+++ account/serializers.py
@@ -8,7 +8,11 @@ from rest_framework.validators import UniqueValidator
 
 class UserSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
-    password = PasswordSerializer()
+
+    password = serializers.CharField(label=_('Password'), write_only=True, validators=[
+        validators.MinLengthValidator(8, _('Ensure password has at least 8 characters.')),
+        validators.MaxLengthValidator(25, _('Ensure password has no more than 25 characters.'))
+    ])
     email = serializers.EmailField(label='Email address', max_length=254, validators=[
         UniqueValidator(queryset=get_user_model().objects.all(),
                         message=_('user with this Email address already exists.'))
@@ -16,12 +20,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'email', 'password')
-
-    def __init__(self, *args, **kwargs):
-        super(UserSerializer, self).__init__(*args, **kwargs)
+        fields = '__all__'
 
     def validate(self, data):
+
         # Never allow updating password on UserSerializer
         if hasattr(self.instance, 'id'):
             if 'password' in data:
@@ -29,32 +31,4 @@ class UserSerializer(serializers.ModelSerializer):
             if 'email' in data:
                 raise serializers.ValidationError(_('Cannot update email'))
         return data
-
-
-class PasswordSerializer(serializers.Serializer):
-    current_password = serializers.CharField(label=_('Current password'))
-    password = serializers.CharField(label=_('Password'), validators=[
-        validators.MinLengthValidator(8, _('Ensure password has at least 8 characters.')),
-        validators.MaxLengthValidator(25, _('Ensure password has no more than 25 characters.'))
-    ])
-    repeat_password = serializers.CharField(label=_('Repeat Password'))
-
-    def validate(self, attrs):
-        current_password = attrs.get('current_password')
-        password = attrs.get('password')
-        repeat_password = attrs.get('repeat_password')
-        if current_password and self.context.get('request').user:
-            # two new passwords match
-            if password != repeat_password:
-                raise serializers.ValidationError(_('Password not matching (New password)'))
-            # current password match
-            credentials = {
-                'email': self.context.get('request').user.email,
-                'password': current_password
-            }
-            if not authenticate(**credentials):
-                raise serializers.ValidationError(_('Current password not matching'))
-        return attrs
-
-
 
