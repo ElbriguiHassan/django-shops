3560414c31eb47e6dd580d5675e2ef387dd0b3ae
ElbriguiHassan <hassanelbrigui@gmail.com>
2018-03-25 00:26:34 +0000

    Fix user manager

--- account/managers.py
+++ account/managers.py
@@ -1,31 +1,26 @@
 from django.contrib.auth.models import BaseUserManager
+from django.db.models import Manager
 
 
-class MyUserManager(BaseUserManager):
-    """
-    A custom user manager to deal with emails as unique identifiers for auth
-    instead of usernames. The default that's used is "UserManager"
-    """
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('The Email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+class UserManager(BaseUserManager, Manager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(
+            email=UserManager.normalize_email(email),
+            password=password,
+            **extra_fields
+        )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, password, **extra_fields)
-
+        user = self.create_user(
+            email=UserManager.normalize_email(email),
+            password=password,
+            is_staff=True,
+            is_superuser=True,
+            **extra_fields
+        )
+        user.save(using=self._db)
+        return user
